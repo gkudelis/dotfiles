5548fc1cd9b9d859c05d067037c2b088f07b94f6
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2021-05-29 12:23:57 +0300

    Convert commands to prepare/clear

--- cli/dotfiles.py
+++ cli/dotfiles.py
@@ -1,9 +1,9 @@
 import click
+import contextlib
 from functools import wraps
-import glob
+from pathlib import Path
 import os
 import sh
-import sys
 
 
 VARIANT_FILENAME = 'current_variant'
@@ -25,11 +25,11 @@ def cli():
 
 @cli.command()
 @with_logging
-def install():
+def prepare():
     variant = get_variant()
     if variant:
         restore_variant(get_variant())
-        fetch_p10k()
+        prepare_p10k()
         link()
         plug_install()
     else:
@@ -38,7 +38,7 @@ def install():
 
 @cli.command()
 @with_logging
-def uninstall():
+def clear():
     plug_uninstall()
     unlink()
     remove_p10k()
@@ -60,20 +60,24 @@ def plug_install():
 
 @with_logging
 def plug_uninstall():
-    plugged_path = os.path.join(os.environ['HOME'], '.config/nvim/plugged')
+    plugged_path = Path.home() / '.config' / 'nvim' / 'plugged'
     sh.rm('-rf', plugged_path)
 
 
 @with_logging
-def fetch_p10k():
+def prepare_p10k():
     repository_url = 'https://github.com/romkatv/powerlevel10k.git'
-    checkout_path = os.path.join(os.environ['HOME'], 'powerlevel10k')
-    sh.git.clone('--depth=1', repository_url , checkout_path)
+    checkout_path = Path.home() / 'powerlevel10k'
+    if checkout_path.is_dir():
+        with cd(checkout_path):
+            sh.git.pull()
+    else:
+        sh.git.clone('--depth=1', repository_url , checkout_path)
 
 
 @with_logging
 def remove_p10k():
-    checkout_path = os.path.join(os.environ['HOME'], 'powerlevel10k')
+    checkout_path = Path.home() / 'powerlevel10k'
     sh.rm('-rf', checkout_path)
 
 
@@ -88,8 +92,9 @@ def unlink():
 
 
 def get_variant():
-    if os.path.exists(VARIANT_FILENAME):
-        with open(VARIANT_FILENAME) as variant_file:
+    variant_path = Path(VARIANT_FILENAME)
+    if variant_path.exists():
+        with variant_path.open() as variant_file:
             return variant_file.readline().strip()
     else:
         return 'default'
@@ -104,14 +109,14 @@ def set_variant(new_variant):
 def common_files():
     for dirpath, _, filenames in os.walk('common'):
         for filename in filenames:
-            fullname = os.path.join(dirpath, filename)
+            fullname = Path(dirpath) / filename
             yield os.path.relpath(fullname, start='common')
 
 
 def restore_file(filename, variant):
-    common_filename = os.path.join('common', filename)
-    current_filename = os.path.join('current', filename)
-    os.makedirs(os.path.dirname(current_filename), exist_ok=True)
+    common_filename = Path('common') / filename
+    current_filename = Path('current') / filename
+    os.makedirs(current_filename.parent, exist_ok=True)
 
     with open(common_filename) as common, open(current_filename, 'w') as current:
         for line in common:
@@ -126,12 +131,12 @@ def restore_file(filename, variant):
 
 
 def resolve_partial_path(partial_name, variant):
-    variant_partial_path = os.path.join('variants', variant, partial_name)
-    default_partial_path = os.path.join('variants', 'default', partial_name)
+    variant_partial_path = Path('variants') / variant / partial_name
+    default_partial_path = Path('variants') / 'default' / partial_name
 
-    if os.path.exists(variant_partial_path):
+    if variant_partial_path.exists():
         return variant_partial_path
-    elif os.path.exists(default_partial_path):
+    elif default_partial_path.exists():
         return default_partial_path
     else:
         return None
@@ -143,3 +148,13 @@ def restore_variant(variant):
     sh.mkdir('current')
     for filename in common_files():
         restore_file(filename, variant)
+
+
+@contextlib.contextmanager
+def cd(path):
+    previous_path = Path.cwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(previous_path)
