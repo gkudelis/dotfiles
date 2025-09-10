c2ff4ea3e364b61b991d19e4cf0c5eb359e8b114
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2025-09-10 15:17:43 +0300

    Change to ruby_lsp

--- common/.config/nvim-kickstart/init.lua
+++ common/.config/nvim-kickstart/init.lua
@@ -492,7 +492,7 @@ require('lazy').setup({
         gopls = {},
         pyright = {},
         rust_analyzer = {},
-        solargraph = {},
+        ruby_lsp = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
