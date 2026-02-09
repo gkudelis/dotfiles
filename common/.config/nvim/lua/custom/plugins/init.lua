8c022168cce1778f64865162f7875610eda34214
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2026-02-10 01:23:39 +0200

    Clean up mappings

--- common/.config/nvim/lua/custom/plugins/init.lua
+++ common/.config/nvim/lua/custom/plugins/init.lua
@@ -9,51 +9,26 @@ return {
     opts = function()
       local metals_config = require('metals').bare_config()
       metals_config.on_attach = function(client, bufnr)
-        local map = function(mode, lhs, rhs, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, lhs, rhs, opts)
+        local map = function(lhs, rhs, desc)
+          vim.keymap.set('n', lhs, rhs, { buffer = bufnr, desc = 'Metals: ' .. desc })
         end
 
-        map('n', 'gD', vim.lsp.buf.definition, { desc = 'Metals: Goto Definition' })
-        map('n', 'K', vim.lsp.buf.hover, { desc = 'Metals: Hover Documentation' })
-        map('n', 'gi', vim.lsp.buf.implementation, { desc = 'Metals: Goto Implementation' })
-        map('n', 'gr', vim.lsp.buf.references, { desc = 'Metals: Goto References' })
-        map('n', 'gds', vim.lsp.buf.document_symbol, { desc = 'Metals: Document Symbols' })
-        map('n', 'gws', vim.lsp.buf.workspace_symbol, { desc = 'Metals: Workspace Symbols' })
-        map('n', '<leader>cl', vim.lsp.codelens.run, { desc = 'Metals: Run Code Lens' })
-        map('n', '<leader>sh', vim.lsp.buf.signature_help, { desc = 'Metals: Signature Help' })
-        map('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Metals: Rename' })
-        map('n', '<leader>f', vim.lsp.buf.format, { desc = 'Metals: Format' })
-        map('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Metals: Code Action' })
-
-        map('n', '<leader>ws', function()
+        -- Metals-specific keymaps (keys that don't overlap with main config)
+        map('<leader>cl', vim.lsp.codelens.run, 'Run Code Lens')
+        map('<leader>ms', vim.lsp.buf.signature_help, 'Signature Help')
+        map('<leader>f', vim.lsp.buf.format, 'Format')
+        map('<leader>mw', function()
           require('metals').hover_worksheet()
-        end, { desc = 'Metals: Hover Worksheet' })
+        end, 'Hover Worksheet')
 
-        -- all workspace diagnostics
-        map('n', '<leader>aa', vim.diagnostic.setqflist, { desc = 'Metals: All Diagnostics' })
-
-        -- all workspace errors
-        map('n', '<leader>ae', function()
+        -- Diagnostics (workspace-level, supplements global <leader>q and [d/]d)
+        map('<leader>aa', vim.diagnostic.setqflist, 'All Diagnostics')
+        map('<leader>ae', function()
           vim.diagnostic.setqflist { severity = vim.diagnostic.severity.ERROR }
-        end, { desc = 'Metals: All Errors' })
-
-        -- all workspace warnings
-        map('n', '<leader>aw', function()
+        end, 'All Errors')
+        map('<leader>aw', function()
           vim.diagnostic.setqflist { severity = vim.diagnostic.severity.WARN }
-        end, { desc = 'Metals: All Warnings' })
-
-        -- buffer diagnostics only
-        map('n', '<leader>d', vim.diagnostic.setloclist, { desc = 'Metals: Buffer Diagnostics' })
-
-        map('n', '[c', function()
-          vim.diagnostic.jump { count = -1, wrap = false }
-        end, { desc = 'Metals: Previous Diagnostic' })
-
-        map('n', ']c', function()
-          vim.diagnostic.jump { count = 1, wrap = false }
-        end, { desc = 'Metals: Next Diagnostic' })
+        end, 'All Warnings')
       end
 
       return metals_config
