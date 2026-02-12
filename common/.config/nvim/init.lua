2be8ab452309cc7c72e5e9f4e936800e70938bad
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2026-02-12 13:47:33 +0200

    Add vim global to fennel

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -510,7 +510,14 @@ require('lazy').setup({
       --  See `:help lsp-config` for information about keys and how to configure
       local servers = {
         -- clangd = {},
-        fennel_language_server = {},
+        fennel_language_server = {
+          settings = {
+            fennel = {
+              diagnostics = { globals = { 'vim' } },
+              workspace = { library = vim.api.nvim_list_runtime_paths() },
+            },
+          },
+        },
         gopls = {},
         pyright = {},
         rust_analyzer = {},
